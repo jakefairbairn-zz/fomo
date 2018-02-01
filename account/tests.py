7d41b04678c10699392ac49d3c8fb2c07492de96
Jake Fairbairn <jake_fairbairn@byu.net>
2018-02-01 00:26:26 -0700

    create/add group/permissions & added fixtures file

--- account/tests.py
+++ account/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission, Group, ContentType
 from account import models as amod
 
 class UserClassTest(TestCase):
+
+    fixtures = [ 'data.yaml' ]
 
     def test_load_save(self):
         '''Test creating, saving, and reloading a user'''
@@ -22,6 +25,9 @@ class UserClassTest(TestCase):
         self.assertEqual(u1.password, u2.password)
         self.assertTrue(u2.check_password('password'))
 
+        superusers_emails = amod.User.objects.filter(is_superuser=True).values_list('email')
+        print(superusers_emails)
+
     def test_field_changes(self):
         '''Test regular field changes (first name, last name, etc.)'''
         u1 = amod.User()
@@ -40,6 +46,29 @@ class UserClassTest(TestCase):
         u3 = amod.User.objects.get(email = 'lisa@simpsons.com')
         self.assertEqual(u2.first_name, u3.first_name)
         self.assertEqual(u2.last_name, u3.last_name)
+
+    def create_assign_group(self):
+        '''Test creating a group'''
+        g1 = Group()
+        g1.name = 'Salespeople'
+        g1.save()
+        self.u1.groups.add(g1)
+        self.u1.save()
+
+        self.assertTrue(self.u1.groups.filter(name='Salespeople'))
+        print("RAN create_assign_group")
+
+    def create_assign_permission(self):
+        p1 = Permission()
+        p.codename = 'change_product_price'
+        p.name = 'Change the price of a product'
+        p.content_type = ContentType.objects.get(id=1)
+        p1.save()
+        print("RAN create_assign_permission")
+
+        #assign permission to gropu
+
+        #assign permission to user
 
     def add_groups(self):
         '''Test adding a few groups'''
